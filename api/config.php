f726de5b867009ddc009d8f9e22387fed7383735
KerimCeljo <kerim.celjo@stu.ibu.edu.ba>
2021-03-29 02:26:11 +0200

    basic dao setup

---
+++ api/config.php
@@ -0,0 +1,17 @@
+<?php
+class Config {
+
+const DB_HOST="localhost";
+const DB_USERNAME="root";
+const DB_PASSWORD="kacket20";
+const DB:SCHEME="carlook";
+
+
+
+}
+
+
+
+
+
+ ?>
---
+++ api/index.php
@@ -0,0 +1,7 @@
+<?php
+
+echo "Heloo from api";
+
+
+
+ ?>
