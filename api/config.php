1dfe8ddf95d0f3651e80d1c2cacefaad95c12b04
KerimCeljo <kerim.celjo@stu.ibu.edu.ba>
2021-04-09 17:30:20 +0200

    working update

--- api/config.php
+++ api/config.php
@@ -3,7 +3,7 @@
 
 class Config {
 
- const DB_HOST="localhost";
+ const DB_HOST="localhost:3307";
  const DB_USERNAME="carlook";
  const DB_PASSWORD="carlook";
  const DB_SCHEME="carlook";
--- api/dao/BaseDao.class.php
+++ api/dao/BaseDao.class.php
@@ -23,10 +23,7 @@ class BaseDao{
 }
 //$this->table=$table;
 
-/*  try {
-  $this->$connection = new PDO("mysql:host=".Config::DB_HOST.";dbname=".Config::DB_SCHEME, Config:: DB_USERNAME, Config::DB_PASSWORD);
-  // set the PDO error mode to exception
-  $this->$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+/*
 
 } catch(PDOException $e) {
   throw $e;
